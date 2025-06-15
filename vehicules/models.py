c21cbb257a5b99e67bf70044e7f38b5b97b4a164
UTurtle <uturtledev@gmail.com>
2025-06-15 11:40:59 +0200

    mise en place supabase

--- vehicules/models.py
+++ vehicules/models.py
@@ -15,15 +15,15 @@ class ChoixTransmission(models.TextChoices):
 class Vehicule(models.Model):
     marque = models.CharField(max_length=50)
     modele = models.CharField(max_length=50)
-    annee = models.IntegerField()
+    annee = models.DateField()
     annee_achat = models.DateField()
     kilometrage_achat = models.IntegerField()
     couleur = models.CharField(max_length=50)
     immatriculation = models.CharField(max_length=9)
     chevaux_fiscaux = models.IntegerField()
     chevaux_din = models.IntegerField()
-    carburant = models.CharField(max_length=50, choices=ChoixCarburant.choices)
-    transmission = models.CharField(max_length=50, choices=ChoixTransmission.choices)
+    carburant = models.CharField(max_length=50, choices=ChoixCarburant.choices, blank=True, null=True)
+    transmission = models.CharField(max_length=50, choices=ChoixTransmission.choices, blank=True, null=True)
     proprietaire = models.ForeignKey(Proprietaire, on_delete=models.CASCADE)
     photo = models.ImageField()
 
