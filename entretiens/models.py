be13740d69153cd8e47ce84087d31639d4c511d9
UTurtle <uturtledev@gmail.com>
2025-07-05 17:41:12 +0200

    validateur pour afficher pdf dans l'admin en dev

--- entretiens/models.py
+++ entretiens/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from vehicules.models import Vehicule
 
 # Create your models here.
+def validateur_extentions(value):
+    if not value.name.endswith('.pdf'):
+        raise ValidationError('Le fichier doit avoir l\'extension .pdf')
+
 class Entretien(models.Model):
     vehicule = models.ForeignKey(Vehicule, on_delete=models.CASCADE)
     date = models.DateField()
     kilometrage = models.IntegerField()
     type_entretien = models.TextField()
-    facture = models.ImageField(upload_to='factures')
+    facture = models.FileField(upload_to='factures', validators = [validateur_extentions])
     montant = models.DecimalField(max_digits=10, decimal_places=2)   
 
     def __str__(self):
-        return f"{self.vehicule} - {self.date}"
+        return f"{self.date} - {self.vehicule} - {self.type_entretien}"
     
     #Montant formaté en euros avec espace en séparateur de milliers et virgule en séparateur de decimal
     def montant_formate(self):
